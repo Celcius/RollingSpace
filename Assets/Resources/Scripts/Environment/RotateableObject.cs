cb6e8ba9cebed1cb9ad4a66cd036bb51c73c774b
Ivo Capelo <ivo.capelo@gmail.com>
2017-01-29 19:05:54 +0000

    Planet Rotation

---
+++ Assets/Resources/Scripts/Environment/RotateableObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateableObject : MonoBehaviour {
+
+    [SerializeField]
+    float rotateSpeed = 100.0f;
+
+    [SerializeField]
+    Transform _reference;
+
+	public void rotate(float xAxis, float yAxis)
+    {
+        if (_reference == null)
+            return;
+        
+        Vector3 up = (_reference.position - transform.position).normalized;
+        Vector3 left = Vector3.Cross(up, Vector3.forward).normalized;
+
+        transform.Rotate(gameObject.transform.InverseTransformDirection(left), yAxis * rotateSpeed);
+        transform.Rotate(gameObject.transform.InverseTransformDirection(-Vector3.forward), xAxis * rotateSpeed);
+    }
+}
---
+++ Assets/Resources/Scripts/Services/OrbitalTestController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitalTestController : MonoBehaviour {
+
+    RotateableObject _obj;
+	// Use this for initialization
+	void Start () {
+        _obj = GetComponent<RotateableObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.A))
+        {
+            _obj.rotate(-1*Time.deltaTime, 0);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            _obj.rotate(1 * Time.deltaTime, 0);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            _obj.rotate(0, 1 * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            _obj.rotate(0, -1 * Time.deltaTime);
+        }
+    }
+}
